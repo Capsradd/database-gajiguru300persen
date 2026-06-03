84cab5a36606fd6ff72e05bef7afc29465505341
Raddin Pratama Rachmat <142906084+Capsradd@users.noreply.github.com>
2026-06-03 21:43:25 +0700

    fix tittle

--- console.php
+++ console.php
@@ -68,7 +68,7 @@ if (isset($_POST['execute_sql'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SQL Console - Pathenol</title>
+    <title>SQL Console - Unidb</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <script>
